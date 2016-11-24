8f71592bb2873036c2f62185bc86df8a3cf1f8f6
Daniel <Danielive@outlook.com>
2016-11-24 15:09:22 +0300

    Update

--- src/main/java/live/daniel/TaskManager/Main.java
+++ src/main/java/live/daniel/TaskManager/Main.java
@@ -1,10 +1,27 @@
 package live.daniel.TaskManager;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by Daniel on 24.11.2016.
  */
-public class Main {
-    public static void main(String arg[]) {
+public class Main extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/mainForm.fxml"));
+        Parent content = loader.load();
+        primaryStage.setTitle("Hello");
+        primaryStage.setScene(new Scene(content, 800, 500));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
---
+++ src/main/java/live/daniel/TaskManager/mainForm.java
@@ -0,0 +1,11 @@
+package live.daniel.TaskManager;
+
+/**
+ * Created by Daniel on 24.11.2016.
+ */
+public class mainForm {
+
+    public void loginButton() {
+        System.out.println("User logged");
+    }
+}
